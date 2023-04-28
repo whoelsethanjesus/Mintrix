786f87306a22bfbd3b53f9c8d570c56a981142cd
Janik <87913738+whoelsethanjesus@users.noreply.github.com>
2023-04-28 18:48:16 +0200

    Add files via upload

---
+++ src/MintrixWeb.h
@@ -0,0 +1,16 @@
+/* * * * * * * * * * * * * *
+ * Mintrix by Janik Heiler *
+ * * * * * * * * * * * * * */
+
+#ifndef MINTRIX_WEBFILES_H
+#define MINTRIX_WEBFILES_H
+namespace MintrixWeb {
+
+WebPage HelloWorld = {
+	R"=====(<h1>Hello World!</h1>)=====",
+	R"=====()=====",
+	R"=====()====="
+};
+
+}
+#endif
