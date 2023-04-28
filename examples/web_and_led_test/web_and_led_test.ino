c88a5847ad8dbbade360226bd9d8ffd4baf3bf02
Janik <87913738+whoelsethanjesus@users.noreply.github.com>
2023-04-28 18:48:41 +0200

    Update web_and_led_test.ino

--- examples/web_and_led_test/web_and_led_test.ino
+++ examples/web_and_led_test/web_and_led_test.ino
@@ -1,1 +1,35 @@
+/* * * * * * * * * * * * * *
+ * Mintrix by Janik Heiler *
+ * * * * * * * * * * * * * */
 
+#include <Mintrix.h>
+#include <MintrixWeb.h>
+
+int i = Mintrix.num_leds - 1;
+unsigned long t0;
+
+void setup() {
+	delay(1000);
+	
+	while(Mintrix.dynamicWiFi_state() < 0) {
+		Mintrix.dynamicWiFi_start();
+		delay(50);
+	}
+	
+	Mintrix.webStart();
+	Mintrix.webPage(MintrixWeb::HelloWorld);
+}
+
+void loop() {
+	Mintrix.loop();
+	
+	// only execute every 200 ms
+ 	if(millis() - t0 <= 200)
+		return;
+    
+	t0 = millis();
+	Mintrix[i] = CRGB(0);
+	i = (i + 1) % Mintrix.num_leds;
+	Mintrix[i] = CRGB(0xffffff);
+	Mintrix.show();
+}
